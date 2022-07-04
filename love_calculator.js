bdfa7f69d8ec82ee95b6df53715386af3f9c1da5
Szymon Harpula <97166816+Synka123@users.noreply.github.com>
2022-07-04 21:54:00 +0200

    Update love_calculator.js

--- love_calculator.js
+++ love_calculator.js
@@ -1,9 +1,6 @@
 var first_name = prompt("Type your name");
 var second_name = prompt("Type your crash name")
-
-function randomn_number(max) {
-  
+function randomn_number(max) { 
   return  Math.floor(Math.random() * max); 
 }
-
-alert("Your match with " + second_name + "is " + randomn_number(10))
+alert("Your match with " + second_name + " is " + randomn_number(101) + "%")

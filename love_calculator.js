7af99e888509175fa6f6caf11968fb63d02bb407
Szymon Harpula <97166816+Synka123@users.noreply.github.com>
2022-07-04 23:28:24 +0200

    Update love_calculator.js

--- love_calculator.js
+++ love_calculator.js
@@ -3,4 +3,9 @@ var second_name = prompt("Type your crash name")
 function randomn_number(max) { 
   return  Math.floor(Math.random() * max); 
 }
-alert("Your match with " + second_name + " is " + randomn_number(101) + "%")
+if (randomn_number(101) >= 50) {
+  alert("Your match with " + second_name + " is " + randomn_number(101) + "%. Your lovescore is high.")  
+}
+else {
+  alert("Your match with " + second_name + " is " + randomn_number(101) + "%. Your love score is low")
+}
